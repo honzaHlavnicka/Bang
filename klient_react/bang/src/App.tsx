f7e41d1edea048d4a1e50686d4e5e0dbe3f81568
honza <jan.hlavnicka.s@gyarab.cz>
2025-10-10 12:19:51 +0200

    refactor: optimize lazy loading of pages in App component and improve code readability

--- klient_react/bang/src/App.tsx
+++ klient_react/bang/src/App.tsx
@@ -1,17 +1,39 @@
-import GamePage from './pages/GamePage';
+import React, { Suspense, useEffect } from "react";
 import { useGame } from './modules/GameContext';
-import LoginPage from './pages/LoginPage';
+import WaitingRoom from "./pages/WaitingRoom";
 
-import BeforeGameWaiting from './pages/BeforeGameWaiting';
+// Lazy importy těžkých stránek
+const GamePage = React.lazy(() => import('./pages/GamePage'));
+const LoginPage = React.lazy(() => import('./pages/LoginPage'));
+const BeforeGameWaiting = React.lazy(() => import('./pages/BeforeGameWaiting'));
 
 function App() {
-  const {gameState} = useGame();
+  const { gameState } = useGame();
+
+  // Přednačítání dalších stránek
+  useEffect(() => {
+    if (!gameState.inGame) {
+      // Pokud je uživatel na LoginPage, přednačti BeforeGameWaiting
+      import('./pages/BeforeGameWaiting');
+    } else if (!gameState.gameStarted) {
+      // Pokud je uživatel na BeforeGameWaiting, přednačti GamePage
+      import('./pages/GamePage');
+    }
+  }, [gameState.inGame, gameState.gameStarted]);
 
   return (
-      <>
-          {gameState.inGame ? (gameState.gameStarted ? <GamePage /> : <BeforeGameWaiting/>) : <LoginPage /> }
-      </>
-  )
+    <Suspense fallback={<WaitingRoom>Náčítání...</WaitingRoom>}>
+      {gameState.inGame ? (
+        gameState.gameStarted ? (
+          <GamePage />
+        ) : (
+          <BeforeGameWaiting />
+        )
+      ) : (
+        <LoginPage />
+      )}
+    </Suspense>
+  );
 }
 
-export default App
+export default App;
