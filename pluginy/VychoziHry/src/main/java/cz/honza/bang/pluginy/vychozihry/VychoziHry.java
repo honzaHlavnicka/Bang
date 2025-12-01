aa99a369bbfd16451d7c0e45a33ef0f6ed8d0af1
honza <jan.hlavnicka.s@gyarab.cz>
2025-12-01 21:21:01 +0100

    Nový projekt pro vyčlenění her.

---
+++ pluginy/VychoziHry/src/main/java/cz/honza/bang/pluginy/vychozihry/VychoziHry.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+
+Toto je domácí verze souborů z programování.
+ */
+
+package cz.honza.bang.pluginy.vychozihry;
+
+/**
+ *
+ * @author honza
+ */
+public class VychoziHry {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
